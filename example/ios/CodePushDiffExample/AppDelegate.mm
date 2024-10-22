ee94013144f6b34ea1098d626f1bb06009ba962d
Ivan Kuchaev <gregoreygonza@gmail.com>
2024-10-22 16:10:17 +0100

    fix: fix ios example

--- example/ios/CodePushDiffExample/AppDelegate.mm
+++ example/ios/CodePushDiffExample/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <CodePush/CodePush.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -24,7 +25,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
